d850c9df1aecfa8eccefcca3e8c85a042966282b
sunyu <1927087714@qq.com>
2020-07-28 22:54:35 +0800

    备份

--- src/com/tank/TankFrame.java
+++ src/com/tank/TankFrame.java
@@ -10,6 +10,15 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame{
 	int x=200,y=300;
 	
+	boolean bL = false;
+	boolean bR= false;
+	boolean bU = false;
+	boolean bD = false;
+	
+	Dir dir=Dir.DOWN;
+	
+	private static final int speed = 10;
+	
 	public TankFrame() {
 		this.setSize(1000,800);
 		this.setResizable(false);
@@ -28,40 +37,78 @@ public class TankFrame extends Frame{
 	@Override
 	public void paint(Graphics g) {
 		g.fillRect(x, y, 20, 20);
+		switch(dir) {
+			case LEFT:
+				x-=speed;
+				break;
+			case RIGHT:
+				x+=speed;
+				break;
+			case UP:
+				y-=speed;
+				break;
+			case DOWN:
+				y+=speed;
+				break;
+			default:break;
+		}
 	}
 	
 	class MyKeyListener extends KeyAdapter{
 		
-		boolean bL = false;//左上
-		boolean bR= false;//右上
-		boolean bU = false;// 左下
-		boolean bD = false;//右下
+		
 		@Override
 		public void keyPressed(KeyEvent e) {
 			int key = e.getKeyCode();
 			 switch(key) {
 			 case KeyEvent.VK_A:
-				 x-=10;
+				 bL=true;
 				 break;
 			 case KeyEvent.VK_D:
-				 x+=10;
+				 bR=true;
 				 break;
 			 case KeyEvent.VK_W:
-				 y-=10;
+				 bU = true;
 				 break;
 			 case KeyEvent.VK_S:
-				 y+=10;
+				bD = true;
 				 break;
 			 default:
 				 break;
 			 }
+			 setMainTankDir();
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			
+			int key = e.getKeyCode();
+			 switch(key) {
+			 case KeyEvent.VK_A:
+				 bL = false;
+				 break;
+			 case KeyEvent.VK_D:
+				 bR = false;
+				 break;
+			 case KeyEvent.VK_W:
+				 bU = false;
+				 break;
+			 case KeyEvent.VK_S:
+				bD = false;
+				 break;
+			 default:
+				 break;
+			 }
+			 setMainTankDir();
 		}
 		
+		private void setMainTankDir() {
+			if(bL) dir = Dir.LEFT;
+			if(bR) dir = Dir.RIGHT;
+			if(bU) dir = Dir.UP;
+			if(bD) dir = Dir.DOWN;
+		}
 	}
-
+	
+	
+	
 }
